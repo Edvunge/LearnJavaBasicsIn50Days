007a05f8a9619e13dd7edf6461cde027aa0a5f4f
edvunge <edvaldolaurentina07@hotmail.com>
2022-04-18 00:47:30 +0100

    update at class AreStaticLocalVariablesAllowedInJava

--- src/day06_day10/variables_comments/AreStaticLocalVariablesAllowedInJava.java
+++ src/day06_day10/variables_comments/AreStaticLocalVariablesAllowedInJava.java
@@ -3,5 +3,27 @@ package day06_day10.variables_comments;
 public class AreStaticLocalVariablesAllowedInJava {
     public static void main(String[] args) {
 
+        System.out.println("As variáveis locais estáticas são permitidas em Java?");
+
+        /*
+Ao contrário do C/C++, as variáveis locais estáticas não são permitidas em Java.
+Por exemplo, o seguinte programa Java falha na compilação com o erro “Variáveis locais estáticas não são permitidas”
+
+class Test {
+   public static void main(String args[]) {
+     System.out.println(fun());
+   }
+
+   static int fun()
+   {
+     static int x= 10;  //Error: Static local variables are not allowed
+     return x--;
+   }
+}
+
+Em Java, uma variável estática é uma variável de classe (para toda a classe).
+Portanto, se tivermos uma variável local estática (uma variável com escopo limitado a função), ela viola o propósito de estática.
+Portanto, o compilador não permite variável local estática.
+ */
     }
 }
