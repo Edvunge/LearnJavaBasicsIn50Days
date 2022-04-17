c8afffce7c6577ad9229205ca8f816a01e717a2b
edvunge <edvaldolaurentina07@hotmail.com>
2022-04-17 18:09:16 +0100

    update package day06_day10

---
+++ src/day06_day10/Exerciciosday07.java
@@ -0,0 +1,36 @@
+package day06_day10;
+
+public class Exerciciosday07 {
+    /*
+    Execícios
+1 Abaixo segue uma lista de valores de diversos tipos primitivos. Identifique o(s) tipo(s) de cada
+um:
+1. “Bom dia”
+2. 3
+3. 235.13
+4. true
+5. -135
+6. 256.23F
+7. ‘A’
+8. 6463275245745L
+
+
+2 Suponha que iremos começar a desenvolver o programa de gerenciamento de mercadorias de
+uma loja. Escreva um código que declare variáveis para representar os seguintes dados: número do
+pedido, código do produto, quantidade e valor total da compra.
+
+
+3 Continuando o exercício anterior, inicialize as variáveis com valores de acordo com o tipo de
+variável que você escolheu em cada declaração.
+
+
+4 Continuando o exercício anterior, imprima na tela o valor de cada variável.
+
+
+Desafios
+1 Olhando para a solução dada nos exercícios acima, você faria alguma alteração caso estivéssemos
+desenvolvendo o sistema para uma loja pequena? E se fosse para uma grande rede de lojas? Quais
+seriam as alterações e quais as implicações?
+
+     */
+}
